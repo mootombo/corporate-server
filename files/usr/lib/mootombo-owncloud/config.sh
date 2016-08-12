a212fde42b4418614d4d06fc39f45566db878924
Lahmizzar Valeryon <lahmizzar@devxive.com>
2016-08-12 21:11:28 +0200

    fix name of archive

--- files/usr/lib/mootombo-owncloud/config.sh
+++ files/usr/lib/mootombo-owncloud/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Custom Vars used in all /usr/lib/mootombo-owncloud scripts
-OCFILE="owncloud_9.1.0.tar.bz2"
+OCFILE="owncloud-9.1.0.tar.bz2"
 OCPATH='/var/www/owncloud'
 OCDATA='/mnt/ocdata'
 HTUSER='www-data'
